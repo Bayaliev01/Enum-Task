d3a6bc84c716887a47b6f5a15fb37db62869ad25
lesson1 <bayaliev07@gmail.com>
2022-08-05 17:41:28 +0300

    rewg

--- src/com/company/Days.java
+++ src/com/company/Days.java
@@ -1,2 +1,25 @@
-package com.company;public enum Days {
+package com.company;
+
+public enum Days {
+    MONDAY("java - Технический урок"),
+    TUESDAY("java - Практика"),
+    WEDNESDAY("java - Технический урок"),
+    THURSDAY("SoftSkills - 2-жумда 1 жолу \n java - Практика"),
+    FRIDAY("java - Технический урок"),
+    SATURDAY("java - Практика онлайн"),
+    SUNDAY("Дем алыш оз алдынча уйдон практика жасоо");
+    String lessons;
+
+
+    @Override
+    public String toString() {
+        return "Days{" +
+                "lessons='" + lessons +
+                super.toString();
+    }
+
+    Days(String lessons ) {
+        this.lessons = lessons;
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,32 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+
+        Days days = Days.valueOf(scanner.next().toUpperCase());
+        if (days.equals(Days.MONDAY) ){
+            System.out.println(Days.MONDAY.lessons);
+        } else if (days.equals(Days.TUESDAY)) {
+            System.out.println(Days.TUESDAY.lessons);
+        } else if (days.equals(Days.WEDNESDAY)) {
+            System.out.println(Days.WEDNESDAY.lessons);
+        } else if (days.equals(Days.THURSDAY)) {
+            System.out.println(Days.THURSDAY.lessons);
+        } else if (days.equals(Days.FRIDAY)) {
+            System.out.println(Days.FRIDAY.lessons);
+        } else if (days.equals(Days.SATURDAY)) {
+            System.out.println(Days.SATURDAY.lessons);
+        } else if (days.equals(Days.SUNDAY)) {
+            System.out.println(Days.SUNDAY.lessons);
+        }
+    }catch (Exception e){
+            System.out.println("Нет таких дней");
+        }
     }
 }
